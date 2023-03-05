bc5343d72130716387437059b64b291948716587
jihwooon <jihwooon@gmail.com>
2023-03-05 17:52:13 +0900

    1번 문제 예제를 생성하라
    1번 문제 파일을 생성했습니다.

---
+++ solution1/app/src/test/java/meetup/solution1.java
@@ -0,0 +1,19 @@
+package meetup;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class solution1 {
+    public String solution(int x) {
+        String answer = "";
+
+        return answer;
+    }
+
+    @Test
+    public void result() {
+        assertThat(solution(5)).isEqualTo("101");
+        assertThat(solution(10)).isEqualTo("1010");
+    }
+}
