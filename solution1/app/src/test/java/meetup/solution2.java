d7ec7558b72a61b9587b0b887df15045cdf0cb35
jihwooon <jihwooon@gmail.com>
2023-03-05 17:52:20 +0900

    2번 문제 예제를 생성하라
    2번 문제 파일을 생성했습니다.

---
+++ solution1/app/src/test/java/meetup/solution2.java
@@ -0,0 +1,19 @@
+package meetup;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class solution2 {
+    public int solution(int n) {
+
+        return 0;
+    }
+
+    @Test
+    void result() {
+        assertThat(solution(10)).isEqualTo(55);
+        assertThat(solution(5)).isEqualTo(5);
+    }
+}
