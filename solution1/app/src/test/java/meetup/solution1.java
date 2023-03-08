695822c958d40107de9b53b320ab203f87065c22
jihwooon <jihwooon@gmail.com>
2023-03-09 00:51:48 +0900

    1번 문제를 해결하라
    TDD 방법으로 문제를 해결했습니다.

--- solution1/app/src/test/java/meetup/solution1.java
+++ solution1/app/src/test/java/meetup/solution1.java
@@ -8,12 +8,23 @@ public class solution1 {
     public String solution(int x) {
         String answer = "";
 
+        if (x >= 1) {
+            answer = x % 2 + answer;
+            return solution(x / 2) + answer;
+        }
+
         return answer;
     }
 
     @Test
     public void result() {
+        assertThat(solution(1)).isEqualTo("1");
+        assertThat(solution(2)).isEqualTo("10");
+        assertThat(solution(3)).isEqualTo("11");
+        assertThat(solution(4)).isEqualTo("100");
         assertThat(solution(5)).isEqualTo("101");
+        assertThat(solution(6)).isEqualTo("110");
+        assertThat(solution(7)).isEqualTo("111");
         assertThat(solution(10)).isEqualTo("1010");
     }
 }
